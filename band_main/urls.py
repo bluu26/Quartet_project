d192f42165f86403e9df41c848f81c9878e3d0ce
bgaszczyk <bloo292@gmail.com>
2024-06-12 18:33:30 +0200

    added some stuff

--- band_main/urls.py
+++ band_main/urls.py
@@ -5,5 +5,9 @@ from band_main import views
 
 urlpatterns = [
     path('createevent/', views.CreateEventView.as_view(), name='create_event'),
+    path('addsong/', views.AddSongView.as_view(), name='add_song'),
+    path('songlist/', views.SongListView.as_view(), name='song_list'),
+    path('successpage/', views.SuccessPageView.as_view(), name='success_page'),
+    path('addorganizator/', views.AddOrganizatorView.as_view(), name='add_organizator'),
 
 ]
--- band_main/views.py
+++ band_main/views.py
@@ -11,6 +11,42 @@ class HomeView(View):
         return render(request, 'home.html')
 
 
+class SuccessPageView(View):
+    def get(self, request):
+        return render(request, 'success_page.html')
+
+
+class AddSongView(View):
+    def get(self, request):
+        return render(request, 'add_song.html')
+
+    def post(self, request):
+        composer = request.POST['composer']
+        name = request.POST['name']
+
+        Song.objects.create(name=name, composer=composer)
+        return redirect('success_page')
+
+
+class SongListView(View):
+    def get(self, request):
+        songs = Song.objects.all()
+        context = {'songs': songs}
+        return render(request, 'song_list.html', context)
+
+
+class AddOrganizatorView(View):
+    def get(self, request):
+        return render(request, 'add_organizator.html')
+
+    def post(self, request):
+        name = request.POST['name']
+        contact = request.POST['contact']
+        description = request.POST['description']
+        Organizator.objects.create(name=name, contact=contact, description=description)
+        return redirect('success_page')
+
+
 class CreateEventView(View):
     def get(self, request):
         songs = Song.objects.all()
@@ -23,10 +59,12 @@ class CreateEventView(View):
         date_start = request.POST.get('date_start')
         time_start = request.POST.get('time_start')
         time_end = request.POST.get('time_end')
-        location = request.POST.get('location')
+        localization = request.POST.get('localization')
         money_profit = request.POST.get('money_profit')
         description = request.POST.get('description')
-        organizator_id = request.POST.get('organizer_id')
+        organizator_id = request.POST.get('organizator_id')
+        leaving_location = request.POST.get('leaving_location')
+        leaving_time = request.POST.get('leaving_time')
 
         organizator = get_object_or_404(Organizator, id=organizator_id)
 
@@ -35,16 +73,18 @@ class CreateEventView(View):
             date_start=date_start,
             time_start=time_start,
             time_end=time_end,
-            location=location,
+            localization=localization,
             money_profit=money_profit,
             description=description,
-            organizator=organizator
+            organizator=organizator,
+            leaving_location=leaving_location,
+            leaving_time=leaving_time
         )
 
         song_ids = request.POST.getlist('songs')
         for song_id in song_ids:
             song = get_object_or_404(Song, id=song_id)
-            event.songs.add(song)
+            event.song.add(song)
 
         return redirect('success_page')
 
