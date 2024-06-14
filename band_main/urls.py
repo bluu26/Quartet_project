de9bab254dc2b2f49d1bee5525f30d287fe5d85b
bgaszczyk <bloo292@gmail.com>
2024-06-14 14:24:11 +0200

    added event and organizator details + tests

--- band_main/urls.py
+++ band_main/urls.py
@@ -5,9 +5,11 @@ from band_main import views
 
 urlpatterns = [
     path('createevent/', views.CreateEventView.as_view(), name='create_event'),
+    path('event_details/<int:pk>', views.EventDetailsView.as_view(), name='event_details'),
     path('addsong/', views.AddSongView.as_view(), name='add_song'),
     path('songlist/', views.SongListView.as_view(), name='song_list'),
     path('successpage/', views.SuccessPageView.as_view(), name='success_page'),
     path('addorganizator/', views.AddOrganizatorView.as_view(), name='add_organizator'),
+    path('organizator_list/', views.OrganizatorListView.as_view(), name='organizator_list'),
 
 ]
--- tests/conftest.py
+++ tests/conftest.py
@@ -48,3 +48,45 @@ def songs():
         lst.append(Song.objects.create(name=i, composer=i))
     return lst
 
+
+@pytest.fixture
+def organizators():
+    lst = []
+    for i in range(5):
+        lst.append(Organizator.objects.create(name=i, contact=i, description=i))
+    return lst
+
+# --------------------------------------------#
+# DETAILS EVENTS
+# --------------------------------------------#
+
+
+@pytest.fixture
+def organizator2():
+    return Organizator.objects.create(name='Organizator 1', contact='Kontakt 1', description='Opis 1')
+
+
+@pytest.fixture
+def songs2():
+    return [
+        Song.objects.create(name='Song 1', composer='Composer 1'),
+        Song.objects.create(name='Song 2', composer='Composer 2')
+    ]
+
+
+@pytest.fixture
+def event2(organizator2, songs2):
+    event2 = Event.objects.create(
+        event_name='Event 1',
+        date_start='2024-01-01',
+        time_start='12:00',
+        time_end='14:00',
+        localization='Lokalizacja 1',
+        money_profit=100.0,
+        description='Opis 1',
+        leaving_location='Miejsce wyjazdu 1',
+        leaving_time='10:00',
+        organizator=organizator2
+    )
+    event2.song.set(songs2)
+    return event2
