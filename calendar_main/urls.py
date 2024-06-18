0a5d08f2c79f067b7194dfe588da38811e53b54a
bgaszczyk <bloo292@gmail.com>
2024-06-18 10:28:36 +0200

    finally working addevent

--- calendar_main/urls.py
+++ calendar_main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import CalendarView, AddEventView
+
+from band_main.views import CreateEventView
+from .views import CalendarView
 
 urlpatterns = [
     path('calendar/<int:year>/<int:month>/', CalendarView.as_view(), name='calendar_view'),
-    path('add_event/<int:day>/<int:month>/<int:year>/', AddEventView.as_view(), name='add_event_view'),
+    # path('add_event/<int:day>/<int:month>/<int:year>/', AddEventView.as_view(), name='add_event_view'),
+    path('createevent/', CreateEventView.as_view(), name='create_event'),
 ]
 
--- calendar_main/views.py
+++ calendar_main/views.py
@@ -52,54 +52,49 @@ class CalendarView(TemplateView):
     def get_event_dict(self, year, month):
         events = Event.objects.filter(date_start__year=year, date_start__month=month)
         event_dict = {}
-
         for event in events:
             day = event.date_start.day
-            day_of_week = event.date_start.strftime('%A')
-
-            if (day, day_of_week) not in event_dict:
-                event_dict[(day, day_of_week)] = []
-
-            event_dict[(day, day_of_week)].append(event.event_name)
-        print(event_dict)
-        print(event_dict[(day, day_of_week)])
+            if day not in event_dict:
+                event_dict[day] = []
+            event_dict[day].append(event)
+        print("Events:", event_dict)
         return event_dict
 
 
-class AddEventView(View):
-    template_name = 'create_event.html'
-
-    def get(self, request, day, month, year):
-        form = EventForm(initial={'date_start': date(year, month, day)})
-        songs = Song.objects.all()
-        organizators = Organizator.objects.all()
-        context = {
-            'form': form,
-            'songs': songs,
-            'organizators': organizators
-        }
-        return render(request, self.template_name, context)
-
-    def post(self, request, day, month, year):
-        form = EventForm(request.POST)
-        if form.is_valid():
-            event = form.save(commit=False)
-            event.date_start = date(year, month, day)  # Ustawienie daty rozpoczęcia na podstawie URL
-            event.save()
-            form.save_m2m()  # Zapisanie relacji Many-to-Many
-
-            # Dodaj utwory do wydarzenia
-            song_ids = request.POST.getlist('songs')
-            for song_id in song_ids:
-                event.song.add(Song.objects.get(id=song_id))
-
-            return redirect('calendar_view', year=year, month=month)
-
-        songs = Song.objects.all()
-        organizators = Organizator.objects.all()
-        context = {
-            'form': form,
-            'songs': songs,
-            'organizators': organizators
-        }
-        return render(request, self.template_name, context)
+# class AddEventView(View):
+#     template_name = 'create_event.html'
+#
+#     def get(self, request, day, month, year):
+#         form = EventForm(initial={'date_start': date(year, month, day)})
+#         songs = Song.objects.all()
+#         organizators = Organizator.objects.all()
+#         context = {
+#             'form': form,
+#             'songs': songs,
+#             'organizators': organizators
+#         }
+#         return render(request, self.template_name, context)
+#
+#     def post(self, request, day, month, year):
+#         form = EventForm(request.POST)
+#         if form.is_valid():
+#             event = form.save(commit=False)
+#             event.date_start = date(year, month, day)  # Ustawienie daty rozpoczęcia na podstawie URL
+#             event.save()
+#             form.save_m2m()  # Zapisanie relacji Many-to-Many
+#
+#             # Dodaj utwory do wydarzenia
+#             song_ids = request.POST.getlist('songs')
+#             for song_id in song_ids:
+#                 event.song.add(Song.objects.get(id=song_id))
+#
+#             return redirect('calendar_view', year=year, month=month)
+#
+#         songs = Song.objects.all()
+#         organizators = Organizator.objects.all()
+#         context = {
+#             'form': form,
+#             'songs': songs,
+#             'organizators': organizators
+#         }
+#         return render(request, self.template_name, context)
